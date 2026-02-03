2670f03ebdafa3117b0a42e12bd8c77016412f39
Bhavesh1677 <pawarbhavesh16@gmail.com>
2026-02-03 13:19:31 +0530

    admin side

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -8,7 +8,6 @@ import 'react-native-reanimated';
 import CartProvider from '@/providers/CartProvider';
 
 import { useColorScheme } from '@components/useColorScheme';
-
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary
@@ -16,7 +15,7 @@ export {
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: 'index',
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -53,7 +52,9 @@ function RootLayoutNav() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <CartProvider>
         <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="index" options={{ title: 'Home' }} />
+          <Stack.Screen name="(admin)" options={{ headerShown: false }} />
+          <Stack.Screen name="(user)" options={{ headerShown: false }} />
           <Stack.Screen name="cart" options={{ presentation: 'modal' }} />
         </Stack>
       </CartProvider>
