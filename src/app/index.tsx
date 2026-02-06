de578cbe6ea3af139ad8a54571304f0d9af45da7
Bhavesh1677 <pawarbhavesh16@gmail.com>
2026-02-06 18:40:03 +0530

    Implement tabbed navigation for admin orders with separate active and archive lists.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,14 @@
 import { View, Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Button from '../components/Button';
 import { Link } from 'expo-router';
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const index = () => {
   return (
+    <SafeAreaProvider>
+      <StatusBar style="auto" />
     <View style={{ flex: 1, justifyContent: 'center', padding: 10 }}>
       <Link href={'/(user)'} asChild>
         <Button text="User" />
@@ -16,6 +20,7 @@ const index = () => {
         <Button text="Sign in" />
       </Link>
     </View>
+    </SafeAreaProvider>
   );
 };
 
